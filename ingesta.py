f285facdd53200870f535c9d640ea311eb8e0193
jhimyj <112188333+jhimyj@users.noreply.github.com>
2024-09-29 19:59:36 -0500

    Add files via upload

---
+++ ingesta.py
@@ -0,0 +1,21 @@
+import pymysql
+import pandas as pd
+import boto3
+
+def export_mysql_to_s3(host, port, user, password, database, table, output_file, bucket_name):
+    connection = pymysql.connect(
+        host=host,
+        port=int(port),
+        user=user,
+        password=password,
+        database=database
+    )
+    df = pd.read_sql(f"SELECT * FROM {table}", connection)
+    df.to_csv(output_file, index=False)
+    connection.close()
+
+    boto3.client('s3').upload_file(output_file, bucket_name, output_file)
+
+export_mysql_to_s3('18.235.6.245', 8005, 'root', 'utec', 'tienda', 'fabricantes', 'data.csv', 'ingestajhimy')
+
+print("Ingesta completada")
