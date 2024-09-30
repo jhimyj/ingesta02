688844fbc60b83a2c680ad31b592111748652ac3
jhimyj <112188333+jhimyj@users.noreply.github.com>
2024-09-29 22:48:36 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,21 +1,18 @@
-import pymysql
 import pandas as pd
 import boto3
+from sqlalchemy import create_engine
 
 def export_mysql_to_s3(host, port, user, password, database, table, output_file, bucket_name):
-    connection = pymysql.connect(
-        host=host,
-        port=int(port),
-        user=user,
-        password=password,
-        database=database
-    )
-    df = pd.read_sql(f"SELECT * FROM {table}", connection)
-    df.to_csv(output_file, index=False)
-    connection.close()
+    try:
+        connection_string = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+        engine = create_engine(connection_string)
+        df = pd.read_sql(f"SELECT * FROM {table}", engine)
+        df.to_csv(output_file, index=False)
+        s3_client = boto3.client('s3')
+        s3_client.upload_file(output_file, bucket_name, output_file)
 
-    boto3.client('s3').upload_file(output_file, bucket_name, output_file)
-
-export_mysql_to_s3('18.235.6.245', 8005, 'root', 'utec', 'tienda', 'fabricantes', 'data.csv', 'ingestajhimy')
-
-print("Ingesta completada")
+        print("Ingesta completada")
+    
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+export_mysql_to_s3('34.201.237.57', 8005, 'root', 'utec', 'tienda', 'fabricantes', 'data.csv', 'ingestajhimy')
